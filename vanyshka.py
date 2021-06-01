2cfd17e2461bc40fcbca88e8838ddab09c0ab4fd
unknown <Alexstavros>
2021-06-01 23:14:21 +0300

    aaa

--- vanyshka.py
+++ vanyshka.py
@@ -1,2 +1,28 @@
-print('hello world')
 print(3 + 3)
+print(3 * 2)
+print(3 / 3)
+print(2 ** 3)
+print(3 // 2)
+print(2 % 3)
+x = 5
+print(x)
+x = "hellow"
+print(x)
+x = 7.9
+type_of_variable = type(x)
+print(type_of_variable)
+x = 10
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x ** y)
+print(x % y)
+credit = 1000
+credit_rate = 10
+number_of_years = 5
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+
+print(final_sum)
